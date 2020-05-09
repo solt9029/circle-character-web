77df06e95fbad4fbf70066b0c53a9f13c1aa370b
Kenshi Shiode <atemu1212@yahoo.co.jp>
2020-05-10 00:20:40 +0900

    Change

--- index.js
+++ index.js
@@ -1,9 +1,3 @@
 $(function () {
-  $("#button").click(function () {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    ctx.font = "400px 'ＭＳ ゴシック'";
-    ctx.textAlign = "center";
-    ctx.strokeText("あ", 400, 400);
-  });
+  $("#button").click(function () {});
 });
